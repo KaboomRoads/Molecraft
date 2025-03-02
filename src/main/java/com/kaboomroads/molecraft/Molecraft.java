0159fe6d4aba562b720aae0a37151a6421d3ed1e
KaboomRoads <helmetdemons@gmail.com>
2025-03-02 21:33:41 +0100

    initial commit

---
+++ src/main/java/com/kaboomroads/molecraft/Molecraft.java
@@ -0,0 +1,11 @@
+package com.kaboomroads.molecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Molecraft implements ModInitializer {
+    public static final String MOD_ID = "molecraft";
+
+    @Override
+    public void onInitialize() {
+    }
+}
